a431d8a31a208ff0cb5016a9852e3c9cd1d74554
HENG, Rattanak Vichboth <116492254+RattanakVicboth@users.noreply.github.com>
2025-04-20 21:10:58 +0700

    Update config.py

--- KSK Travel/backend/config.py
+++ KSK Travel/backend/config.py
@@ -7,15 +7,15 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
     
     # MySQL database configuration
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'Dambo123')
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', '')
+    MYSQL_USER = os.environ.get('MYSQL_USER', '')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
     MYSQL_DB = os.environ.get('MYSQL_DB', 'user_db')
     
     # ==== Flask‑Mail settings ====
-    MAIL_SERVER   = 'smtp.gmail.com'        # or your SMTP host
+    MAIL_SERVER   = ''        # or your SMTP host
     MAIL_PORT     = 587
     MAIL_USE_TLS  = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # your SMTP user
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # your SMTP password
-    MAIL_DEFAULT_SENDER = ('KSK Travel Contact', 'no-reply@ksktravel.com')
+    MAIL_DEFAULT_SENDER = ('', '')
